286648b38844fa65b8ac2a0ba1cf3974e43e0a1e
hieule <hieult1996@gmail.com>
2024-08-10 22:56:07 -0500

    jax acceleration flags

--- evolved_latent/cli.py
+++ evolved_latent/cli.py
@@ -6,6 +6,15 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"  # to avoid memory fragmentation
 # os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 
+# Jax acceleration flags
+os.environ["XLA_FLAGS"] = (
+    "--xla_gpu_enable_triton_softmax_fusion=true "
+    "--xla_gpu_triton_gemm_any=false "
+    "--xla_gpu_enable_async_collectives=true "
+    "--xla_gpu_enable_latency_hiding_scheduler=true "
+    "--xla_gpu_enable_highest_priority_async_stream=true "
+)
+
 import importlib.resources
 import argparse
 import evolved_latent
